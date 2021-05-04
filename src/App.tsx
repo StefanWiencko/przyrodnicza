8a874cb7cbf35ecbc2d1381a4685fc41e7191d88
turgor <stefanwiencko2@gmail.com>
2021-05-04 16:44:36 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { LandingPage } from "./components/LandingPage/LandingPage";
+import "./style/main.scss";
+function App() {
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
